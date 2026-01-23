67a154b0943b7adcba796cba79a56fafc50f0139
aditi04menon <aditimenon004@gmail.com>
2026-01-23 21:18:35 +0000

    Revert meme helper to use MEME_URL

--- src/helpers/meme.js
+++ src/helpers/meme.js
@@ -1,18 +1,8 @@
 // helpers/meme.js
 import { EmbedBuilder } from "discord.js";
-import axios from 'axios';
+
+const MEME_URL = process.env.MEME_URL || "";
 
 export const getMeme = async () => {
-  try {
-    const response = await axios.get('https://meme-api.com/gimme');
-    const data = response.data;
-    return new EmbedBuilder()
-      .setImage(data.url)
-      .setTitle(data.title);
-  } catch (error) {
-    console.error(error);
-    return new EmbedBuilder()
-      .setTitle('Error')
-      .setDescription('Could not fetch a meme right now.');
-  }
+  return new EmbedBuilder().setImage(MEME_URL);
 };
